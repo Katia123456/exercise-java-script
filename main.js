b253142a9424ce6e0194481aa045947f02c2c6ef
Katia123456 <katia.mlhem@hotmail.com>
2023-05-31 22:16:09 +0200

    kkjjjhjk

---
+++ main.js
@@ -0,0 +1,23 @@
+const name = "David"; // ✅ Good!
+console.log("name");
+const lastname = "Bruno"; // ❌ the keyword to declare the variable is missing
+console.log("lastname");
+let présentation =
+  "Hi, I'm David Bruno from SF, I like to cook and meet new people."; // ❌ the variable name is not explicit. (this is David's biography)
+console.log(présentation);
+let img1 = "http://www.go.com/davif.png"; // ❌ the variable name is not explicit and the keyword is missing (this is David's profile picture)
+console.log(img1);
+let ville = "San Francisco"; // ❌ the variable name is not explicit and the keyword is missing (try to guess what "San Francisco" could refer to)
+
+let followers = 109; // ❌ the keyword to declare the variable is missing
+console.log(followers);
+followers = 200; // ❌ the keyword to declare the variable is missing
+console.log(followers);
+// 🏁 Exercise 02 - David is following one more account increment the total of following account
+
+présentation =
+  "David is following one more account increment the total of following account";
+console.log(présentation);
+
+
+
